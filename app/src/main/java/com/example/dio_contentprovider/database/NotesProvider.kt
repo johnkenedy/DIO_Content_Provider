99730f0f87ffa2dbaec1e894e0eaecf9b218dbe2
John <john.kenedy232@gmail.com>
2021-07-14 19:42:58 -0300

    Initial commit

---
+++ app/src/main/java/com/example/dio_contentprovider/database/NotesProvider.kt
@@ -0,0 +1,71 @@
+package com.example.dio_contentprovider.database
+
+import android.content.ContentProvider
+import android.content.ContentValues
+import android.content.Context
+import android.content.UriMatcher
+import android.database.Cursor
+import android.database.sqlite.SQLiteDatabase
+import android.media.UnsupportedSchemeException
+import android.net.Uri
+import android.provider.BaseColumns._ID
+import com.example.dio_contentprovider.database.NotesDatabaseHelper.Companion.TABLE_NOTES
+
+class NotesProvider : ContentProvider() {
+
+    private lateinit var mUriMatcher: UriMatcher
+    private lateinit var dbHelper: NotesDatabaseHelper
+    override fun onCreate(): Boolean {
+        mUriMatcher = UriMatcher(UriMatcher.NO_MATCH)
+        mUriMatcher.addURI(AUTHORITY, "notes", NOTES)
+        mUriMatcher.addURI(AUTHORITY, "notes/#", NOTES_BY_ID)
+
+        if (context != null) {
+            dbHelper = NotesDatabaseHelper(context as Context)
+        }
+
+        return true
+    }
+
+    override fun delete(uri: Uri, selection: String?, selectionArgs: Array<String>?): Int {
+        if (mUriMatcher.match(uri) == NOTES_BY_ID) {
+            val db: SQLiteDatabase = dbHelper.writableDatabase
+            val linesAffected = db.delete(TABLE_NOTES, "$_ID =?", arrayOf(uri.lastPathSegment))
+            db.close()
+            context?.contentResolver?.notifyChange(uri, null)
+            return linesAffected
+        } else {
+            throw  UnsupportedSchemeException("Invalid Uri to delete")
+        }
+    }
+
+    override fun getType(uri: Uri): String? = throw UnsupportedSchemeException("Uri not implemented.")
+
+    override fun insert(uri: Uri, values: ContentValues?): Uri? {
+        TODO("Implement this to handle requests to insert a new row.")
+    }
+
+    override fun query(
+        uri: Uri, projection: Array<String>?, selection: String?,
+        selectionArgs: Array<String>?, sortOrder: String?,
+    ): Cursor? {
+        TODO("Implement this to handle query requests from clients.")
+    }
+
+    override fun update(
+        uri: Uri, values: ContentValues?, selection: String?,
+        selectionArgs: Array<String>?,
+    ): Int {
+        TODO("Implement this to handle requests to update one or more rows.")
+    }
+
+    companion object {
+        const val AUTHORITY = "com.example.dio_contentprovider.database.provider"
+        val BASE_URI = Uri.parse("content://$AUTHORITY")
+        val URI_NOTES = Uri.withAppendedPath(BASE_URI, "notes")
+
+        const val NOTES = 1
+        const val NOTES_BY_ID = 2
+    }
+
+}
